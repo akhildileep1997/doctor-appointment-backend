867c15dc8396248ed5d9750acc3ab9cd1b19bca7
akhildileep1997 <akhildileep1997@gmail.com>
2023-10-26 18:47:38 +0530

    doctor-appintment-backend

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const jsonserver = require('json-server')
+
+const server = jsonserver.create()
+
+const router = jsonserver.router('db.json')
+
+const middleware = jsonserver.defaults()
+
+const port = process.env.port || 3001
+
+server.use(middleware)
+
+server.use(router)
+
+
+server.listen(port, ()=>{
+    console.log('server is listening in port'+port);
+})
